6de3002bc51731f77155c7fee986d280c6921952
Harold Pulcher <pulcher@pulcher.biz>
2021-10-25 20:54:51 -0500

    initial

---
+++ parseSomeJson/InspireEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace parseSomeJson
+{
+    public class InspireEntry
+    {
+        public int Id { get; set; }
+        public string Quote { get; set; }
+        public string Source { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ parseSomeJson/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace parseSomeJson
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var client = new HttpClient();
+
+            var stuff = await client.GetFromJsonAsync<QuoteHolder>("https://raw.githubusercontent.com/bmumz/inspirational-quotes-api/main/db.json");
+
+            var count = stuff.Quotes.Count;
+
+            var junk = string.Empty;
+        }
+
+
+    }
+
+    internal class QuoteHolder
+    {
+        public List<InspireEntry> Quotes { get; set; }
+    }
+}
